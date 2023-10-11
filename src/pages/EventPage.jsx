f4498938a5f7cd8e20f7fe94a75781804599b240
Bart Croijmans <bcroijmans@gmail.com>
2023-10-11 11:15:51 +0200

    EventPage commit

--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -1,6 +1,42 @@
-import React from 'react';
-import { Heading } from '@chakra-ui/react';
+import React, { useEffect, useState } from "react";
+import { Box, Image, Flex, Badge, Text, Button } from "@chakra-ui/react";
+import { useParams } from "react-router-dom";
 
 export const EventPage = () => {
-  return <Heading>Event</Heading>;
+  const [event, setEvent] = useState({});
+  const { eventId } = useParams();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`http://localhost:3000/events/${eventId}`);
+      const data = await response.json();
+      setEvent(data);
+    };
+    fetchData();
+  }, [eventId]);
+
+  return (
+    <Box key={event.id} p="5" shadow="md" borderWidth="1px">
+      <Image borderRadius="md" src={event.image} />
+      <Flex align="baseline" mt={2}>
+        <Badge colorScheme="pink">Plus</Badge>
+        <Text
+          ml={2}
+          textTransform="uppercase"
+          fontSize="sm"
+          fontWeight="bold"
+          color="pink.800"
+        >
+          {event.title}
+        </Text>
+      </Flex>
+      <Text mt={2}>{event.description}</Text>
+      <Button colorScheme="blue" onClick={() => alert("Edit event clicked!")}>
+        Edit event
+      </Button>
+      <Button colorScheme="red" onClick={() => alert("Delete event clicked!")}>
+        Delete event
+      </Button>
+    </Box>
+  );
 };
